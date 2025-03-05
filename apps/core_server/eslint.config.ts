812e7dba34c414c0e231f8abd8a4d3796c0d0543
Oleg Vasechko <oleg.vasechko@artezio-training.com0>
2025-03-05 12:08:35 +0300

    FIX:eslint config

---
+++ apps/core_server/eslint.config.ts
@@ -0,0 +1,19 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = [
+    {
+      files: ['*.js', '*.ts'],
+      languageOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        globals: {
+          node: 'readonly',
+          es2020: 'readonly', 
+        },
+      },
+      rules: {
+        'no-console': 'warn',
+        'no-unused-vars': 'warn',
+      },
+    },
+  ];
+  
