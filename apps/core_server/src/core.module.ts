7f17c8674d0c9ea9b27fa50cb436385b46f0d2be
innoPract <aleh.vasechka@innowise.com>
2025-03-05 12:59:50 +0300

    feat(IIH-#22,#24,#19): import/export questions (#87)
    1)migrate from fastify to express
    2)use nest-neo4j instead neode
    3) fix global filter exceptions
    4)fix eslint config problems
    5)add swagger documentation (api/docs)
    6)create questions resource
    7)create endpoint to get all questions ( GET : api/qiestions)
    8) create endpoints to import from file (multipart/form-data) and static import from server folder (POST : api/questions/import, api/questions/import/static )
    9)create export endpoint to save data from db.
    ---------
    
    Co-authored-by: Oleg Vasechko <oleg.vasechko@artezio-training.com0>

--- apps/core_server/src/core.module.ts
+++ apps/core_server/src/core.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
+import { QuestionsModule } from './questions/questions.module';
+import { Neo4jModule } from 'nest-neo4j';
+import { ConfigModule } from '@nestjs/config';
 
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     DatabaseModule,
+    QuestionsModule,
+    Neo4jModule,
   ],
   controllers: [UserController],
   providers: [UserService],
