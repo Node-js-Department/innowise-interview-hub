e529b74628606fbd455a8f9827f8e56ed9a75154
msatala1 <michal.satala@innowise.com>
2025-03-05 12:09:45 +0100

    fix: changed logo to innoviewHub

--- apps/client/src/app/(unprotected)/signin/page.tsx
+++ apps/client/src/app/(unprotected)/signin/page.tsx
@@ -119,8 +119,8 @@ const RightSection = () => {
 const SigninCard = () => {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-y-4 justify-center items-center">
-      <Image src="/logo.webp" alt="logo" height={56} width={302} />
-      <h2 className="text-3xl font-bold text-gray-800">
+      <Image src="/logo.png" alt="logo" height={66} width={352} />
+      <h2 className="text-3xl font-bold text-gray-800 mt-4">
         Innautomation <span className="text-gray-600">Toolset</span>
       </h2>
       <GoogleButton />
