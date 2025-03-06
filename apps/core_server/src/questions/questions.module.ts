58fb9ef833b5b17b217396b29575be7c4579b428
Oleg Vasechko <oleg.vasechko@artezio-training.com0>
2025-03-06 11:08:42 +0300

    FIX:questions.module configure

--- apps/core_server/src/questions/questions.module.ts
+++ apps/core_server/src/questions/questions.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
+import { Neo4jModule } from 'nest-neo4j';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ImportService } from './import.service';
 import { ExportService } from './export.service';
-import { HttpModule } from "@nestjs/axios";
-import { DatabaseModule } from '@/database/database.module';
+import {HttpModule} from "@nestjs/axios";
 
 @Module({
   imports: [
-    DatabaseModule,
     HttpModule,
+    ConfigModule,
+    Neo4jModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        scheme: configService.get<string>('NEO4J_SCHEME', 'neo4j'),
+        host: configService.get<string>('NEO4J_HOST', 'localhost'),
+        port: configService.get<number>('NEO4J_BOLT_PORT', 7687),
+        username: configService.get<string>('NEO4J_USERNAME', 'neo4j'),
+        password: configService.get<string>('NEO4J_PASSWORD', 'password'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [QuestionsController],
   providers: [QuestionsService, ImportService, ExportService],
